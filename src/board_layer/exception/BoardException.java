1bd309ed0b5f398beb2149547fd5121a2e4c5abc
MATHEUS HENRIQUE DE MACEDO <67766992+nrick16@users.noreply.github.com>
2021-07-14 12:18:36 -0300

    Delete src/board_layer directory

--- src/board_layer/exception/BoardException.java
+++
@@ -1,9 +0,0 @@
-package board_layer.exception;
-
-public class BoardException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-	public BoardException(String msg) {
-		super(msg);
-	}
-}
